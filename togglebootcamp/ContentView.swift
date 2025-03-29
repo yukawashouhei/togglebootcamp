4a4db444c1c9975ae54c0117be355055b80f6c3f
Shohei <yukawa.shouhei@gmail.com>
2025-03-29 13:20:30 +0900

    Initial Commit

---
+++ togglebootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  togglebootcamp
+//
+//  Created by 湯川昇平 on 2025/03/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ togglebootcamp/togglebootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  togglebootcampApp.swift
+//  togglebootcamp
+//
+//  Created by 湯川昇平 on 2025/03/29.
+//
+
+import SwiftUI
+
+@main
+struct togglebootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
