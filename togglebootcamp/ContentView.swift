84d7fad296baeb5c5de797be8ec8cc25bddfc5f0
Shohei <yukawa.shouhei@gmail.com>
2025-03-30 12:12:23 +0900

    ColorPickerの追加。

--- togglebootcamp/ContentView.swift
+++ togglebootcamp/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 enum Flavor: String, CaseIterable, Identifiable {
     case chocolate, vanilla, strawberry
-
+    
     var id: String { self.rawValue }
     var description: String { self.rawValue.capitalized }
 }
@@ -12,16 +12,17 @@ struct ContentView: View {
     @State var selection: String = "1"
     @State private var volume: Double = 50.0
     @State private var selectedFlavor: Flavor = .chocolate
-
+    @State private var backgroundColor: Color = .green
+    
     let options = [1, 2, 3, 4, 5]
-
+    
     @State private var selectedOption = 1
-
+    
     var body: some View {
         VStack {
             Text("Toggle is: \(isToggleOn ? "On" : "Off")")
                 .padding(.bottom)
-
+            
             // toggleの機能
             VStack {
                 HStack {
@@ -29,20 +30,20 @@ struct ContentView: View {
                     Text(isToggleOn ? "Online" : "Offline")
                 }
                 .font(.title)
-
+                
                 Toggle(isOn: $isToggleOn) {
                     Text("Toggle Label")
                 }
                 .tint(.purple)
             }
             .padding(.horizontal, 100)
-
+            
             VStack(alignment: .leading) { // ここに閉じ括弧を追加
-
+                
                 // Pickerの機能
                 Text("Custom Style")
                     .font(.headline)
-
+                
                 Picker(
                     selection: $selectedOption,
                     label: Text("Custom Options")
@@ -53,16 +54,16 @@ struct ContentView: View {
                 }
                 .pickerStyle(.segmented)
                 .padding(.horizontal, 100)
-
+                
                 Slider(value: $volume,
                        in: 0...100,
                        minimumValueLabel: Image(systemName: "speaker"),
                        maximumValueLabel: Image(systemName: "speaker.wave.3"),
                        label: {
-                           Text("Volume")
-                       }
+                    Text("Volume")
+                }
                 )
-
+                
                 Form { // Form を VStack の中に移動
                     Picker("Flavor", selection: $selectedFlavor) {
                         ForEach(Flavor.allCases) { flavor in
@@ -76,6 +77,17 @@ struct ContentView: View {
             .padding(.horizontal, 100)
             .frame(maxWidth: .infinity)
             Spacer()
+            
+            ZStack { // ZStack を VStack の中に移動
+                Color.red
+                    .edgesIgnoringSafeArea(.all)
+                
+                ColorPicker(
+                    "Select a color",
+                    selection: $backgroundColor,
+                    supportsOpacity: true // supportsOpacity に引数を渡す
+                )
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.systemGray6))
